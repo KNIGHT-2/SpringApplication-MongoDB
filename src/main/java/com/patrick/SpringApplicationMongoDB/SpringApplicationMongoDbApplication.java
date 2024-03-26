073ad78a59c6d5865c78ed009178e82936fbd966
Patrick M <patrickmacedo02@gmail.com>
2024-03-26 12:25:11 -0300

    Project created

---
+++ src/main/java/com/patrick/SpringApplicationMongoDB/SpringApplicationMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.patrick.SpringApplicationMongoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApplicationMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApplicationMongoDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/patrick/SpringApplicationMongoDB/SpringApplicationMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patrick.SpringApplicationMongoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApplicationMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
