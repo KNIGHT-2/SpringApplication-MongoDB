314b07f718a2d0f4c2502c58f9d89ef1c4f85025
Patrick M <patrickmacedo02@gmail.com>
2024-03-26 16:30:53 -0300

    Connecting on MongoDB - UserRepository - UserService

--- src/main/java/com/patrick/SpringApplicationMongoDB/resources/UserResource.java
+++ src/main/java/com/patrick/SpringApplicationMongoDB/resources/UserResource.java
@@ -1,28 +1,24 @@
 package com.patrick.SpringApplicationMongoDB.resources;
 
 import com.patrick.SpringApplicationMongoDB.domain.User;
+import com.patrick.SpringApplicationMongoDB.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
     @GetMapping
     public ResponseEntity<List<User>> findAll(){
-        User test = new User("1", "Test User", "test@email.com");
-        User test2 = new User("2", "Test User2", "test2@email.com");
-
-        List<User> list = new ArrayList<>();
-
-        list.addAll(Arrays.asList(test, test2));
-
+        List<User> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
 }
