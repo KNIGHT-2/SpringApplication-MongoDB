190916231c149e72df7f658274bf8e3957743d99
Patrick M <patrickmacedo02@gmail.com>
2024-04-01 16:25:34 -0300

    User insert

--- src/main/java/com/patrick/SpringApplicationMongoDB/services/UserService.java
+++ src/main/java/com/patrick/SpringApplicationMongoDB/services/UserService.java
@@ -1,10 +1,12 @@
 package com.patrick.SpringApplicationMongoDB.services;
 
+import com.patrick.SpringApplicationMongoDB.DTO.UserDTO;
 import com.patrick.SpringApplicationMongoDB.domain.User;
 import com.patrick.SpringApplicationMongoDB.repository.UserRepository;
 import com.patrick.SpringApplicationMongoDB.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 import java.util.Optional;
@@ -21,6 +23,14 @@ public class UserService {
 
     public User findById(String id){
         User obj = repository.findById(id).orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+        return obj;
+    }
+
+    public User insert(User obj){
+        return repository.insert(obj);
+    }
+    public User fromDTO(UserDTO dto){
+        User obj = new User(dto.getId(),dto.getName(),dto.getEmail());
         return obj;
     }
 }
