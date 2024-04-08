d94d3cb278713944ad945d8fabd6a37953dbcc0a
Patrick M <patrickmacedo02@gmail.com>
2024-04-08 11:08:47 -0300

    GET request with query methods

--- src/main/java/com/patrick/SpringApplicationMongoDB/resources/PostResource.java
+++ src/main/java/com/patrick/SpringApplicationMongoDB/resources/PostResource.java
@@ -1,14 +1,14 @@
 package com.patrick.SpringApplicationMongoDB.resources;
 
+import com.patrick.SpringApplicationMongoDB.resources.util.URL;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.patrick.SpringApplicationMongoDB.domain.Post;
 import com.patrick.SpringApplicationMongoDB.services.PostService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -20,5 +20,11 @@ public class PostResource {
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Post> findById(@PathVariable String id){
 		return ResponseEntity.ok().body(service.findById(id));
+	}
+
+	@GetMapping(value = "/titlesearch")
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text){
+		text = URL.decodeParam(text);
+		return ResponseEntity.ok().body(service.findByTitle(text));
 	}
 }
--- src/main/java/com/patrick/SpringApplicationMongoDB/services/PostService.java
+++ src/main/java/com/patrick/SpringApplicationMongoDB/services/PostService.java
@@ -1,7 +1,10 @@
 package com.patrick.SpringApplicationMongoDB.services;
 
 import java.io.Serializable;
+import java.util.List;
 
+import com.patrick.SpringApplicationMongoDB.DTO.AuthorDTO;
+import com.patrick.SpringApplicationMongoDB.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,10 +18,14 @@ public class PostService implements Serializable{
 	
 	@Autowired
 	private PostRepository repository;
-	
+
 	public Post findById(String id) {
 		Post obj = repository.findById(id).orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 		return obj;
 	}
-	
+
+	public List<Post> findByTitle(String text){
+		return repository.findByTitleContainingIgnoreCase(text);
+	}
+
 }
